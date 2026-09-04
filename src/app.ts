6d0e5cc953cc7e2e35b5d07e70fb50dc7e03e839
el perro <marcelo.guevara1983@gmail.com>
2026-09-04 18:42:56 -0500

    change cors policy

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app=express()
 
 
 app.use(cors({
-    origin:['http://localhost:5173','https://budgetcalculator-three.vercel.app/'],
+    origin:['http://localhost:5173','https://budgetcalculator-three.vercel.app'],
     methods:['GET','POST'],
     credentials:true
 }))
